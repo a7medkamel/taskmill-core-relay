d37175d4c5118cde783272ed9d2ed23adebba26a
Ahmed Kamel <ak@ahmedkamel.net>
2016-03-22 17:49:01 -0700

    support onebox, use random-id, fix error obj

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+"use strict";
+
 var Relay = require('./lib/relay');
+
+function main(options) {
+  options = options || {};
+
+  let relay = new Relay(options);
+
+  return relay
+          .listen({ port : options.port })
+          .return(relay);
+}
+
+function instance() {
+  return Relay.get();
+}
 
 module.exports = {
     Relay     : Relay
+  , instance  : instance
+  , main      : main
 };
