3ab152fc9f1bb009380850a698e6efd4e3acf013
Ahmed Kamel <ak@ahmedkamel.net>
2016-08-19 00:12:55 -0700

    gateway rework

--- index.js
+++ index.js
@@ -2,22 +2,6 @@
 
 var Relay = require('./lib/relay');
 
-function main(options) {
-  options = options || {};
-
-  let relay = new Relay(options);
-
-  return relay
-          .listen({ port : options.port })
-          .return(relay);
-}
-
-function instance() {
-  return Relay.get();
-}
-
 module.exports = {
-    Relay     : Relay
-  , instance  : instance
-  , main      : main
+    Relay : Relay
 };
